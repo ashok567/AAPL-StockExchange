68e6cecfe58e3a4270c8f46e5e18a4a5ec9ccc80
Ashok Thakur <iashok180@gmail.com>
2019-08-22 14:07:02 +0530

    simple time series chart added

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -5,9 +5,15 @@ from mpl_finance import candlestick_ohlc
 from matplotlib.dates import date2num
 from datetime import datetime
 
-
 def convert_dates(x):
     return datetime.strptime(x, "%d-%m-%Y").strftime("%Y-%m-%d")
+
+def settings():
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Apple Stock Exchange')
+    plt.xticks(rotation=40)
+    plt.tight_layout()
 
 df = pd.read_csv('HistoricalQuotes-3Months.csv')
 df['date'] = pd.to_datetime(df['date'].apply(convert_dates))
@@ -16,13 +22,15 @@ ohlc = df[['date', 'open', 'high', 'low', 'close']].copy()
 ohlc['date'] = ohlc['date'].apply(date2num)
 
 fig, ax = plt.subplots()
+
 candlestick_ohlc(ax, ohlc.values, width=0.6, colorup='g', colordown='r')
 
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.title('Apple Stock Exchange')
 ax.xaxis_date()
-plt.xticks(rotation=40)
-plt.tight_layout()
-plt.show()
+settings()
 plt.savefig('ohlc.png')
+plt.show()
+
+plt.plot(df['date'], df['close'])
+settings()
+plt.savefig('time series.png')
+plt.show()
